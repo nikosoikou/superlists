f0d64d098938efa0bc478f9adea9700ffb675209
nicolaosoikou@gmail.com <nicolaosoikou@gmail.com>
2019-08-15 16:05:00 +0200

    Correct test name

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,6 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
