534955c8414cb370f52dd3f37d78dc0cd0fdff92
nicolaosoikou@gmail.com <nicolaosoikou@gmail.com>
2019-08-20 17:30:05 +0200

    Add a fabfile for automated deploys

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -26,19 +26,19 @@ def _update_settings(source_folder, site_name):
     settings_path = source_folder + '/superlists/settings.py'
     sed(settings_path, 'DEBUG = True', 'DEBUG = False')
     sed(settings_path,
-        'ALLOWED_HOSTS = .+$',
-        "ALLOWED_HOSTS = [f'{site_name}']"
+        'ALLOWED_HOSTS =.+$',
+        f'ALLOWED_HOSTS = ["{site_name}"]'
         )
-    secret_key_file = source_folder + 'superlists/secret_key.py'
+    secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join([random.SystemRandom().choice(chars) for _ in range(50)])
-        append(secret_key_file, f"SECRET_KEY = '{key}'")
+        append(secret_key_file, f'SECRET_KEY = "{key}"')
     append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
 
 def _update_virtualenv(source_folder):
-    virtualenv_folder = source_folder + '../virtualenv'
+    virtualenv_folder = source_folder + '/../virtualenv'
     if not exists(virtualenv_folder + '/bin/pip'):
         run(f'python3.7 -m venv {virtualenv_folder}')
     run(f'{virtualenv_folder}/bin/pip install -r {source_folder}/requirements.txt')
@@ -47,7 +47,7 @@ def _update_virtualenv(source_folder):
 def _update_static_files(source_folder):
     run(
         f'cd {source_folder}'
-        ' && ../virtualenv/bin/python manage.py collectstatic'
+        ' && ../virtualenv/bin/python manage.py collectstatic --no-input'
     )
 
 
